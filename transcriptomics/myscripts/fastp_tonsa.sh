d9168d887ba003f8fd8d037ae429cdb0e3afe8d9
Nicole Phelan <nmphelan@node400.cluster>
2025-10-09 17:16:04 -0400

    fastp clean reads & salmon

--- transcriptomics/myscripts/fastp_tonsa.sh
+++ transcriptomics/myscripts/fastp_tonsa.sh
@@ -3,7 +3,7 @@
 #---------  Slurm preamble, defines the job with #SBATCH statements
 
 # Give your job a name that's meaningful to you, but keep it short
-#SBATCH --job-name=fastp
+#SBATCH --job-name=fastp_transcriptomics
 
 # Name the output file: Re-direct the log file to your home directory
 # The first part of the name (%x) will be whatever you name your job 
@@ -15,7 +15,7 @@
 # Specify when Slurm should send you e-mail.  You may choose from
 # BEGIN, END, FAIL to receive mail, or NONE to skip mail entirely.
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=netid@uvm.edu
+#SBATCH --mail-user=nicole.phelan@uvm.edu
 
 # Run on a single node with four cpus/cores and 8 GB memory
 #SBATCH --nodes=1
@@ -31,6 +31,10 @@
 
 # Below here, give you bash script with your list of commands
 
+
+# Below here, give you bash script with your list of commands
+
+
 # This script loops through a set of files defined by MYSAMP, matching left and right reads
 # and cleans the raw data using fastp according to parameters set below
 
@@ -39,36 +43,36 @@ module purge
 module load gcc fastp
 
 # Define the path to the transcriptomics folder in your Github repo.
-MYREPO="/users/n/m/nmphelan/projects/eco_genomics_2025/transcriptomics"
+MYREPO="/users/n/m/nmphelan/projects/eco_genomics_2025/"
 
 # make a new directory within myresults/ to hold the fastp QC reports
-mkdir ${MYREPO}/myresults/fastp_reports
+mkdir ${MYREPO}/transcriptomics/myresults/fastp_reports
 
 # cd to the location (path) to the fastq data:
 
 cd /gpfs1/cl/ecogen/pbio6800/Transcriptomics/RawData
 
 # Define the sample code to anlayze
-# Be sure to replace with your 4-digit sample code
+# Be sure to replace with your 2-digit sample code
 
-MYSAMP="C2R"
+MYSAMP="C2"
 
 # for each file that has "MYSAMP" and "_1.fq.gz" (read 1) in the name
 # the wildcard here * allows for the different reps to be captured in the list
 # start a loop with this file as the input:
 
-for READ1 in ${MYSAMP}*1.fq.gz
+for READ1 in ${MYSAMP}*_R1*.gz
 do
 
 # the partner to this file (read 2) can be found by replacing the _1.fq.gz with _2.fq.gz
 # second part of the input for PE reads
 
-READ2=${READ1/_1.fq.gz/_2.fq.gz}
+READ2=${READ1/_R1*.gz/_R2*.gz}
 
 # make the output file names: print the fastq name, replace _# with _#_clean
 
-NAME1=$(echo $READ1 | sed "s/_1/_1_clean_NP/g")
-NAME2=$(echo $READ2 | sed "s/_2/_2_clean_NP/g")
+NAME1=$(echo $READ1 | sed "s/_R1/_R1_cleanNP/g")
+NAME2=$(echo $READ2 | sed "s/_R2/_R2_cleanNP/g")
 
 # print the input and output to screen 
 
@@ -76,7 +80,9 @@ echo $READ1 $READ2
 echo $NAME1 $NAME2
 
 # call fastp
-fastp -i ${READ1} -I ${READ2} -o /gpfs1/cl/ecogen/pbio6800/Transcriptomics/cleandata/${NAME1} -O /gpfs1/cl/ecogen/pbio6800/Transcriptomics/cleandata/${NAME2} \
+fastp -i ${READ1} -I ${READ2} \
+-o /gpfs1/cl/ecogen/pbio6800/Transcriptomics/cleandata/${NAME1} \
+-O /gpfs1/cl/ecogen/pbio6800/Transcriptomics/cleandata/${NAME2} \
 --detect_adapter_for_pe \
 --trim_poly_g \
 --thread 4 \
@@ -84,6 +90,6 @@ fastp -i ${READ1} -I ${READ2} -o /gpfs1/cl/ecogen/pbio6800/Transcriptomics/clean
 --cut_window_size 6 \
 --qualified_quality_phred 20 \
 --length_required 35 \
---html ~/myresults/fastqc/${NAME1}.html
+--html ${MYREPO}/transcriptomics/myresults/fastp_reports/${NAME1}.html
 
 done
---
+++ transcriptomics/transcriptomics_notes.md
@@ -0,0 +1,21 @@
+# Transcriptomics Module Notes
+
+## Fall 2025 Ecological Genomics
+
+## Author: Nicole Phelan
+
+This is a place for me to keep my notes on my electronic/server activities during the transcriptomics module.
+
+### 10/7/2025 Getting started with the module
+
+-   On the first day of transcriptomics, I learned about the motivation and process of asking questions and analyzing RNA sequence data.
+
+-   I started to run fastp to clean and visualize the data quality in my fastq files, but I ran into file recognition issues.
+
+    -   To be continued on Thursday...
+
+### 10/9/2025 Running fastp
+
+-   I copied the fastp_tonsa.sh script from the `gpfs1/cl/ecogen/pbio6800/Transcriptomics/scripts` folder to `~/projects/eco_genomics_2025/transcriptomics/myscripts`.
+
+    -   I edited the file to set my population to C2 and set my directory to my repository.
